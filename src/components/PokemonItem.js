078653b20e70ddc1e0b6e26b7b98569cb9e4358c
naru <naruyoshi514.tri@gmail.com>
2023-11-28 11:00:35 +0900

    taiwind組み込み完了

--- src/components/PokemonItem.js
+++ src/components/PokemonItem.js
@@ -6,12 +6,16 @@ function PokemonItem({ pokemon }) {
   return (
     <div className="pokemon-item">
       <Link to={`/pokemon/${pokemon.name}`}>
-        <img className="t-pokemon_img" src={pokemon.image} alt={pokemon.name} />
-        <p className="t-pokemon_number p-5">{`#${pokemon.id
+        <img
+          className="t-pokemon_img w-full"
+          src={pokemon.image}
+          alt={pokemon.name}
+        />
+        <p className="t-pokemon_number">{`#${pokemon.id
           .toString()
           .padStart(4, "0")}`}</p>
-        <p className="t-poekmon_name">{pokemon.name}</p>
-        <div className="pokemon-types">
+        <p className="t-poekmon_name mt-1 text-2xl font-bold">{pokemon.name}</p>
+        <div className="pokemon-types ">
           {pokemon.types.map((type, index) => (
             <span key={index} className={`type ${type}`}>
               {type.toUpperCase()}
@@ -19,9 +23,6 @@ function PokemonItem({ pokemon }) {
           ))}
         </div>
       </Link>
-      <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-        Click me
-      </button>
     </div>
   );
 }
